55779588edf1a4e6df860b595fa9653c3df8ba10
Sachee <sacheeghimire@gmail.com>
2026-03-17 11:38:46 +0545

    Storing multiple elements using list [try/catch]

---
+++ listTryCatch.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import sync_playwright
+text_alert = []
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    context = browser.new_context()
+    page = context.new_page()
+
+    page.goto('https://demo.automationtesting.in/Selectable.html')
+
+    # Store multiple elements using list
+
+    elements = page.query_selector_all('b')
+    print(len(elements))
+    for i in elements:
+        print(i.text_content())
+    page.wait_for_timeout(5000)
