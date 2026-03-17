41d005d334abe86ba1d9560f27d055fe5a579719
Sachee <sacheeghimire@gmail.com>
2026-03-17 17:58:05 +0545

    Screenshot and video recording of the website for review during web_automation

---
+++ video_screenShot.py
@@ -0,0 +1,21 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+
+    # Video directory
+    context = browser.new_context(record_video_dir='./videos')
+    page = browser.new_page()
+
+    page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+    page.wait_for_selector('//input[@name="username"]').fill('Admin')
+    page.wait_for_selector('//input[@name="password"]').fill('admin123')
+
+# screenshot method and path
+    page.screenshot(path='./screenshots/loginpage.png')
+
+    page.wait_for_selector('//button[@type="submit"]').click()
+
+    page.screenshot(path='./screenshots/homepage.png')
+
+    context.close()
